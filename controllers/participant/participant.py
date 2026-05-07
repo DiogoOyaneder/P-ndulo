8a517f83903747c0ae583d5ddb8040b407422a49
DiogoOyaneder <diogo.oyaneder@gmail.com>
2026-05-07 19:40:24 +0100

    final commit

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -23,9 +23,9 @@ rightMotor.setVelocity(0.0)
 maxSpeed = min(rightMotor.getMaxVelocity(), leftMotor.getMaxVelocity())
 
 # Define the PID control constants and variables.
-KP = 31.4
-KI = 100.5
-KD = 0
+KP = 150
+KI = 10
+KD = 1
 integral = 0.0
 previous_position = 0.0
 
